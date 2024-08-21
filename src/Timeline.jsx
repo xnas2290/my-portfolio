af7d4d39edfae221db43d3af468add7e5efcd24a
Haddaoui_anas <anas123haddaoui@gmail.com>
2024-08-21 12:07:13 +0100

    fixing small screen

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -1,11 +1,34 @@
+// import React from 'react';
+// import TimelineItem from './TimelineItem';
+
+// const Timeline = ({ items }) => {
+//   return (
+//     <div className="container mx-auto w-full h-full">
+//       <div className="relative wrap overflow-hidden p-10 h-full">
+//         <div className="sm:border-2-2 absolute border-opacity-20 border-gray-700 h-full border" style={{ left: '50%' }}></div>
+//         {items.map((item, index) => (
+//           <TimelineItem key={index} item={item} index={index} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Timeline;
+
+
+
+
+
+
 import React from 'react';
 import TimelineItem from './TimelineItem';
 
 const Timeline = ({ items }) => {
   return (
-    <div className="container mx-auto w-full h-full">
+    <div className="container mx-auto w-full h-full px-4 sm:px-0">
       <div className="relative wrap overflow-hidden p-10 h-full">
-        <div className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border" style={{ left: '50%' }}></div>
+        <div className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border sm:left-1/2 sm:-translate-x-1/2"></div>
         {items.map((item, index) => (
           <TimelineItem key={index} item={item} index={index} />
         ))}
@@ -15,3 +38,4 @@ const Timeline = ({ items }) => {
 };
 
 export default Timeline;
+
--- src/TimelineItem.jsx
+++ src/TimelineItem.jsx
@@ -1,20 +1,45 @@
+// import React from 'react';
+
+// const TimelineItem = ({ item, index }) => {
+//   const isEven = index % 2 === 0;
+
+//   return (
+//     <div className={`mb-8 flex justify-between ${isEven ? 'flex-row-reverse' : 'items-center'} w-full`}>
+//       <div className="order-1 w-3/12"></div>
+//       <div className={` flex items-center order-1 dark:bg-gray-600 bg-gray-800 shadow-xl px-5 h-8 rounded-full ${isEven ? 'm-10' : ''}`}>
+//         <h1 className="mx-auto font-semibold text-lg text-white ">{item.date}</h1>
+//       </div>
+//       <div className={`order-1 ${isEven ? 'mr-auto' : 'ml-auto'} bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4`}>
+//         <h3 className="mb-3 font-bold text-gray-800 text-xl">{item.title}</h3>
+//         <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{item.description}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TimelineItem;
 import React from 'react';
 
 const TimelineItem = ({ item, index }) => {
   const isEven = index % 2 === 0;
 
   return (
-    <div className={`mb-8 flex justify-between ${isEven ? 'flex-row-reverse' : 'items-center'} w-full`}>
-      <div className="order-1 w-3/12"></div>
-      <div className={` flex items-center order-1 dark:bg-gray-600 bg-gray-800 shadow-xl px-5 h-8 rounded-full ${isEven ? 'm-10' : ''}`}>
-        <h1 className="mx-auto font-semibold text-lg text-white ">{item.date}</h1>
-      </div>
-      <div className={`order-1 ${isEven ? 'mr-auto' : 'ml-auto'} bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4`}>
+    <div className={`mb-8 flex flex-col sm:flex-row justify-between ${isEven ? 'sm:flex-row-reverse' : 'items-center'} w-full`}>
+
+      <div className={`order-2 sm:order-2 w-full sm:w-5/12 sm:${isEven ? 'mr-auto' : 'ml-auto'} bg-gray-400 rounded-lg shadow-xl px-6 py-4`}>
         <h3 className="mb-3 font-bold text-gray-800 text-xl">{item.title}</h3>
         <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">{item.description}</p>
+      </div>
+      <div className={`order-1 sm:order-1 w-full sm:w-3/12 mb-2 sm:mb-0`}>
+        <div className={`flex items-center dark:bg-gray-600 bg-gray-800 shadow-xl px-5 h-8 rounded-full `}>
+          <h1 className="mx-auto font-semibold text-lg text-white">{item.date}</h1>
+        </div>
       </div>
     </div>
   );
 };
 
 export default TimelineItem;
+
+
+
